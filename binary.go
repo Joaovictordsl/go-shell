fd612e190739eba4aea3ff5b4c1b78d5f9e3c7aa
Joao Victor de Sousa Lima <joaovictordsl00@gmail.com>
2026-02-11 18:09:05 -0300

    go files

---
+++ binary.go
@@ -0,0 +1,21 @@
+package main
+
+func Binary(array []int, target int) int {
+	low := 0
+	high := len(array) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+		guess := array[mid]
+
+		if guess == target {
+			return mid
+		} else if target < guess {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+
+	return -1
+}
--- main.go
+++ main.go
@@ -2,28 +2,8 @@ package main
 
 import "fmt"
 
-func binary(array []int, target int) int {
-	low := 0
-	high := len(array) - 1
-
-	for low <= high {
-		mid := (low + high) / 2
-		guess := array[mid]
-
-		if guess == target {
-			return mid
-		} else if target < guess {
-			high = mid - 1
-		} else {
-			low = mid + 1
-		}
-	}
-
-	return -1
-}
-
 func main() {
 	array := []int{1, 2, 3, 4, 30, 50, 99, 200, 350, 460}
 
-	fmt.Println(binary(array, 350))
+	fmt.Println(Binary(array, 350))
 }
