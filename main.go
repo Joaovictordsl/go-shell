a4947842647fb9239650d79fa82d6986f3b8f302
Joao Victor de Sousa Lima <joaovictordsl00@gmail.com>
2026-02-13 18:25:17 -0300

    API

--- main.go
+++ main.go
@@ -1,9 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+	Status  int    `json:"status"`
+}
+
+var database = []Response{}
 
 func main() {
-	array := []int{1, 2, 3, 4, 30, 50, 99, 200, 350, 460}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
 
-	fmt.Println(Binary(array, 350))
+		if r.Method == "GET" {
+			json.NewEncoder(w).Encode(database)
+			return
+		}
+
+		if r.Method == "POST" {
+			var newRes Response
+
+			err := json.NewDecoder(r.Body).Decode(&newRes)
+			if err != nil {
+				http.Error(w, "JSON invalido", http.StatusBadRequest)
+				return
+			}
+
+			database = append(database, newRes)
+
+			w.WriteHeader(http.StatusCreated)
+			json.NewEncoder(w).Encode(newRes)
+
+			return
+		}
+	})
+
+	fmt.Println("Listening and Serving at localhost:8080")
+
+	http.ListenAndServe(":8080", nil)
 }
