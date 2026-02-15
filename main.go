ea2417cb023d76358278392da648e5b6132d1111
Joao Victor de Sousa Lima <joaovictordsl00@gmail.com>
2026-02-15 18:07:39 -0300

    go api

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+	Status  int    `json:"status"`
+}
+
+var database = []Response{}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		if r.Method == "GET" {
+			json.NewEncoder(w).Encode(database)
+			return
+		}
+
+		if r.Method == "POST" {
+			var newRes Response
+		}
+	})
+
+	fmt.Println("Listening and serving at localhost:8080")
+
+	http.ListenAndServe(":8080", nil)
+}
